d5a5f8939777a27ee4b85e5e8c242bc27b69359d
puerarongo <akhardin96@gmail.com>
2023-03-12 16:58:39 +0200

    add path on render

--- src/helpers/urlPath.ts
+++ src/helpers/urlPath.ts
@@ -1,3 +1,4 @@
-const BASE_URL = "http://localhost:8080/api";
+//const BASE_URL = "http://localhost:8080/api";
+const BASE_URL = "https://new-post-back-poshta.onrender.com/api";
 
 export default BASE_URL;
