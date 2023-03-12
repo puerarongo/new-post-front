76efb90873bc88b5aff62f6c0e236324c3a1aba4
puerarongo <akhardin96@gmail.com>
2023-03-12 13:11:11 +0200

    add view moer button

--- src/components/departments/Departments.tsx
+++ src/components/departments/Departments.tsx
@@ -1,12 +1,20 @@
 import React from "react";
 import styles from "./Departments.module.css";
 import FormDepartments from "./formDepartments/FormDepartments";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import DepartmentsItem from "./departmentsItem/DepartmentsItem";
 import IDataDepartments from "../../helpers/interface/dataDepartments.interface";
+import { getPlaceData } from "../../redux/operation/place-operation";
 
 const Departments: React.FC = () => {
+  const dispatch = useDispatch();
   const place = useSelector((state: any) => state.place);
+
+  const viewMore = () => {
+    const page = Math.floor(place.departments.length / 50) + 1;
+    dispatch(getPlaceData({ city: place.city, page }));
+    console.log(place.departments.length);
+  };
 
   return (
     <div className={styles.container}>
@@ -32,6 +40,15 @@ const Departments: React.FC = () => {
                   <h2>There are no departments available in this city!</h2>
                 </li>
               )}
+              <li>
+                <button
+                  className={styles.button}
+                  type="button"
+                  onClick={viewMore}
+                >
+                  View More
+                </button>
+              </li>
             </ul>
           ) : !place.city && place.message ? (
             <div className={styles.start__container}>
